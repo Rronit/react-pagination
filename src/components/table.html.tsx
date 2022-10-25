1d943c2c2419ca8f12c4d20a950291ab1053f852
Rronit <rronit2015@gmail.com>
2022-10-25 09:47:34 +0530

    feat: table with search added

--- src/components/table.html.tsx
+++ src/components/table.html.tsx
@@ -1,9 +1,57 @@
 import React from "react";
+import "./table.style.css";
 
-const TableHtml = ({ list }: { list: any[] }) => {
+const TableHtml = ({
+  list,
+  handleSearch
+}: {
+  list: any[];
+  handleSearch: (e: any) => void;
+}) => {
   console.log(list);
 
-  return <div>here is the html part.......</div>;
+  return (
+    <>
+      <input
+        type="search"
+        id="tsearch"
+        name="tsearch"
+        placeholder="Search by name, email or role"
+        onChange={handleSearch}
+      ></input>
+      <table>
+        <tr>
+          <th>
+            <input
+              type="checkbox"
+              id="header"
+              name="header"
+              value="All"
+            ></input>
+          </th>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Role</th>
+          <th>Action</th>
+        </tr>
+        {list.map((item, ind) => (
+          <tr key={ind}>
+            <td>
+              <input
+                type="checkbox"
+                id={item.email}
+                name={item.email}
+                value={item.email}
+              ></input>
+            </td>
+            <td>{item.name}</td>
+            <td>{item.email}</td>
+            <td>{item.role}</td>
+          </tr>
+        ))}
+      </table>
+    </>
+  );
 };
 
 export default React.memo(TableHtml);
--- src/components/table.logic.tsx
+++ src/components/table.logic.tsx
@@ -4,6 +4,9 @@ import TableHtml from "./table.html";
 
 export const TableLogic = () => {
   const [list, setList] = useState<any[]>([]);
+  const [listToRender, setListToRender] = useState<any[]>([]);
+  const [globalSearchList, setGlobalSearchList] = useState<any[]>([]);
+  const [globalSearchText, setGlobalSearchText] = useState<string>("");
   const getData = async () => {
     let resp = (
       await axios.get(
@@ -11,13 +14,32 @@ export const TableLogic = () => {
       )
     ).data;
     setList(resp);
+    setListToRender(resp.slice(0, 10));
   };
 
   useEffect(() => {
-    console.log(">>>>>>>>>");
-
     getData();
   }, []);
+  const handleSearch = (e: any) => {
+    const text = e.target.value;
+    setGlobalSearchText(text);
+    if (text.length === 0) {
+      setListToRender(list.slice(0, 10));
+      return;
+    }
 
-  return <TableHtml list={list} />;
+    // console.log(e.target.value);
+    let result = list.filter(
+      it =>
+        it.role.includes(text) ||
+        it.name.includes(text) ||
+        it.email.includes(text)
+    );
+    console.log(result);
+    setGlobalSearchList(result);
+    if (result.length > 10) setListToRender(result.slice(0, 10));
+    else setListToRender(result);
+  };
+
+  return <TableHtml list={listToRender} handleSearch={handleSearch} />;
 };
